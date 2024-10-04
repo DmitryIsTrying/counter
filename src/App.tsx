7db289c05f1b0e554db260a0318d2ab9f5e842de
Dmitry <d2013.dima@mail.ru>
2024-10-04 17:45:11 +0500

    Fix random num

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,58 @@
-import React, { useEffect, useState} from 'react';
-import './App.css';
-import {IncrementsSettings} from "./components/incrementsSettings/IncrementsSettings";
-import {Counter} from "./components/counter/Counter";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { IncrementsSettings } from "./components/incrementsSettings/IncrementsSettings";
+import { Counter } from "./components/counter/Counter";
 
 export type IncrementsObj = {
-    startInc: number;
-    endInc: number;
-}
+  startInc: number;
+  endInc: number;
+};
 
 function App() {
+  const [increments, setIncrements] = useState<IncrementsObj>({
+    startInc: 1,
+    endInc: 0,
+  });
 
-    const [increments, setIncrements] = useState<IncrementsObj>({
-        startInc: 0,
-        endInc: 0,
-    })
+  const [counter, setCounter] = useState<number>(0);
 
-    const [counter, setCounter] = useState<number>(0);
+  const [randomNum, setRandomNum] = useState<number>(
+    Math.floor(Math.random() * 10) + 1
+  );
 
-    const [randomNum, setRandomNum] = useState<number>(Math.ceil(Math.random() * 9));
+  const [isOn, setIsOn] = useState<boolean>(true);
 
-    const [isOn, setIsOn] = useState<boolean>(true);
+  useEffect(() => {
+    setIncrements({
+      startInc: Number(localStorage.getItem("startInc") || 0),
+      endInc: Number(localStorage.getItem("endInc") || 0),
+    });
+  }, []);
 
-    useEffect(() => {
-        setIncrements({startInc: Number(localStorage.getItem('startInc') || 0), endInc: Number(localStorage.getItem('endInc') || 0)});
-    }, [])
+  useEffect(() => {
+    localStorage.setItem("startInc", JSON.stringify(increments.startInc));
+    localStorage.setItem("endInc", JSON.stringify(increments.endInc));
+  }, [increments]);
 
-    useEffect(() => {
-        localStorage.setItem('startInc', JSON.stringify(increments.startInc))
-        localStorage.setItem('endInc', JSON.stringify(increments.endInc))
-    }, [increments])
-
-
-    return (
-        <div className="App">
-            <IncrementsSettings setIncrements={setIncrements} increments={increments} isOn={isOn} setIsOn={setIsOn} setCounter={setCounter}/>
-            <Counter setCounter={setCounter} increments={increments} isOn={isOn} counter={counter} setRandomNum={setRandomNum} randomNumber={randomNum}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <IncrementsSettings
+        setIncrements={setIncrements}
+        increments={increments}
+        isOn={isOn}
+        setIsOn={setIsOn}
+        setCounter={setCounter}
+      />
+      <Counter
+        setCounter={setCounter}
+        increments={increments}
+        isOn={isOn}
+        counter={counter}
+        setRandomNum={setRandomNum}
+        randomNumber={randomNum}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,60 +1,85 @@
-import React from 'react';
-import {Container} from "../container/Container";
-import {CountParagraph} from "../countParagraph/CountParagraph";
+import React from "react";
+import { Container } from "../container/Container";
+import { CountParagraph } from "../countParagraph/CountParagraph";
 import styles from "../countParagraph/counterParagraph.module.css";
-import {Button} from "../button/Button";
-import {IncrementsObj} from "../../App";
-import cont from '../container/container.module.css'
+import { Button } from "../button/Button";
+import { IncrementsObj } from "../../App";
+import cont from "../container/container.module.css";
 
 type CounterPropsType = {
-    isOn: boolean,
-    randomNumber: number,
-    counter: number,
-    increments: IncrementsObj
-    setRandomNum: (randomNumber: number) => void
-    setCounter: (counter: number) => void
-}
+  isOn: boolean;
+  randomNumber: number;
+  counter: number;
+  increments: IncrementsObj;
+  setRandomNum: (randomNumber: number) => void;
+  setCounter: (counter: number) => void;
+};
 
-export const Counter = ({isOn, randomNumber, counter, increments, setRandomNum, setCounter}: CounterPropsType) => {
+export const Counter = ({
+  isOn,
+  randomNumber,
+  counter,
+  increments,
+  setRandomNum,
+  setCounter,
+}: CounterPropsType) => {
+  // Handlers functions
 
-    // Handlers functions
+  function handleUpCounter() {
+    setCounter(counter + 1);
+  }
 
-    function handleUpCounter() {
-        setCounter(counter + 1)
-    }
+  function handleResetCounter() {
+    let random = Math.floor(Math.random() * 10) + 1;
+    setRandomNum(random);
+    setCounter(isOn ? 0 : increments.startInc);
+  }
 
-    function handleResetCounter() {
-        let random = Math.ceil(Math.random() * 9)
-        setRandomNum(random)
-        setCounter(isOn ? 0 : increments.startInc)
-    }
+  // end
 
+  //  styles logic
 
-    // end
+  const classLogicForCounterParagraphRight =
+    counter >= increments.endInc ? styles.disabled : styles.active;
+  const classLogicForCounterParagraphLeft =
+    counter >= randomNumber ? styles.disabled : styles.active;
+  const classLogicForCounterParagraphLast = !isOn
+    ? classLogicForCounterParagraphRight
+    : classLogicForCounterParagraphLeft;
+  const classForCounterParagraph =
+    styles.counter + " " + classLogicForCounterParagraphLast;
 
-    //  styles logic
+  // end
 
-    const classLogicForCounterParagraphRight = counter >= increments.endInc ? styles.disabled : styles.active
-    const classLogicForCounterParagraphLeft = counter >= randomNumber ? styles.disabled : styles.active
-    const classLogicForCounterParagraphLast = !isOn ?  classLogicForCounterParagraphRight : classLogicForCounterParagraphLeft
-    const classForCounterParagraph = styles.counter + ' ' + classLogicForCounterParagraphLast
-
-    // end
-
-
-    return (
-        <Container>
-            <CountParagraph className={styles.maxValue}>Max
-                value: {isOn ? randomNumber : increments.endInc}</CountParagraph>
-            <CountParagraph
-                className={classForCounterParagraph}>{counter}</CountParagraph>
-            <progress className={cont.progress} value={counter} max={isOn ? randomNumber : increments.endInc}></progress>
-            <div className={cont.btnContainer}>
-                <Button disabled={isOn ? counter >= randomNumber : counter >= increments.endInc}
-                        onClick={handleUpCounter}>inc</Button>
-                <Button disabled={isOn ? counter === 0 : counter === increments.startInc}
-                        onClick={handleResetCounter}>reset</Button>
-            </div>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <CountParagraph className={styles.maxValue}>
+        Max value: {isOn ? randomNumber : increments.endInc}
+      </CountParagraph>
+      <CountParagraph className={classForCounterParagraph}>
+        {counter}
+      </CountParagraph>
+      <progress
+        className={cont.progress}
+        value={counter}
+        max={isOn ? randomNumber : increments.endInc}
+      ></progress>
+      <div className={cont.btnContainer}>
+        <Button
+          disabled={
+            isOn ? counter >= randomNumber : counter >= increments.endInc
+          }
+          onClick={handleUpCounter}
+        >
+          inc
+        </Button>
+        <Button
+          disabled={isOn ? counter === 0 : counter === increments.startInc}
+          onClick={handleResetCounter}
+        >
+          reset
+        </Button>
+      </div>
+    </Container>
+  );
+};
